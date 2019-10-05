cdf395637fe84e899e9cd1917b89bc4e5c6e15df
amaurymagalhaes <amaurymagalhaesf@hotmail.com>
2019-10-05 07:46:39 -0300

    pag com calendario n funcional

--- src/pages/arena.js
+++ src/pages/arena.js
@@ -1,30 +1,130 @@
 import React, { Component } from 'react';
 
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Button, Image, TouchableOpacity, Platform } from 'react-native';
+import {CalendarList, LocaleConfig} from 'react-native-calendars';
 
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 import xMark from '../../assets/xmark.png';
 
+import Star from '../../assets/greenstar.png';
+
+import Placeholder from '../../assets/greenplaceholder.png';
+
+import Coin from '../../assets/greencoin.png';
+
+import moment from 'moment';
+
+
 export default class Arena extends Component {
+    constructor(props) {
+        super(props);
+        const DISABLED_DAYS = ['Sábado', 'Domingo']
+        this.state = {
+            today: new Date(),
+            dateSelected: null,
+            markedDates: this.getDaysInMonth(moment().month(), moment().year(),  DISABLED_DAYS)
+        };
+
+      }
+    
+      
+      
+
+      selectionData = (_day) => {
+        console.log(this.state);
+        this.setState(
+            this.state.dateSelected = {[_day.dateString]:{selected: true, selectedColor: '#7C038C'}}
+          )
+        console.log(this.state.markedDates)
+      }
+
+      getDaysInMonth(month, year, days) {
+        let pivot = moment().month(month).year(year).startOf('month')
+        const end = moment().month(month).year(year).endOf('month')
+    
+        let dates = {}
+        const disabled = { disabled: true, disableTouchEvent: false }
+        while(pivot.isBefore(end)) {
+          days.forEach((day) => {
+            dates[pivot.day(day).format("YYYY-MM-DD")] = disabled
+          })
+          pivot.add(7, 'days')
+        }
+    
+        return dates
+      }
+
   render() {
-    return(
+    return(      
     <View style={styles.background}>
-        <View style={styles.superiorItems}>
-            <TouchableOpacity style={styles.xMarkBox}>
-                <Image source={xMark} style={styles.xMark}/>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.favoriteBackground}>
-            <Text style={styles.favoriteText}>Favorito</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.backgroundImage}>
-        </View>
-        <Text style={styles.textArenaName}>Arena</Text>
-        <View style={{}}>
-        <Text style={styles.textArenaProps}>5,0</Text>
-        <Text style={styles.textArenaProps}>5,0</Text>
-        <Text style={styles.textArenaProps}>5,0</Text>
+        <View style={styles.allItems}>
+            <View style>
+                <View style={styles.superiorItems}>
+                    <TouchableOpacity style={styles.xMarkBox}>
+                        <Image source={xMark} style={styles.xMark}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.favoriteBackground}>
+                    <Text style={styles.favoriteText}>Favorito</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.backgroundArenaImage}>
+                </View>
+                <Text style={styles.textArenaName}>Arena</Text>
+                <View style={{flexDirection: "row", alignSelf: 'center', margin: hp('1%')}}>
+                    <Image source={Star} style={styles.imageArenaProps}/>
+                    <Text style={styles.textArenaProps}>5,0</Text>
+                    <Image source={Coin} style={styles.imageArenaProps}/>
+                    <Text style={styles.textArenaProps}>$$$</Text>
+                    <Image source={Placeholder} style={styles.imageArenaProps}/>
+                    <Text style={styles.textArenaProps}>1 km</Text>
+                </View>
+            </View>
+
+            <View style={styles.middleItems}>
+            <CalendarList
+            onDayPress={(day) => {console.log('selected day', day)}}
+            pastScrollRange={0}
+            minDate={this.state.today}
+            futureScrollRange={3}
+            scrollEnabled={true}
+            onMonthChange={(date) => {
+                this.setState({
+                  markedDates: this.getDaysInMonth(date.month - 1, date.year, DISABLED_DAYS)
+                })
+              }}
+            showScrollIndicator={true}
+            onDayPress={(day) => {this.selectionData(day)}}
+            onDayLongPress={(day) => {this.selectionData(day)}}
+            //disabledByDefault
+            theme={{
+                calendarBackground: '#07A24A',
+                textSectionTitleColor: '#7C038C',
+                monthTextColor: 'white',
+                textMonthFontSize: hp('2.5%'),
+                dotColor: '#00adf5',
+                selectedDotColor: '#000000',
+                dayTextColor: 'white',
+                todayTextColor: 'purple',
+                textDisabledColor: '#AAAAAA',
+            }}
+            markedDates={this.state.dateSelected,
+                        this.state.markedDates}
+            />
+            </View>
+
+            <View style={styles.inferiorItems}>
+                <View style={{flexDirection: 'column', justifyContent: 'center', marginLeft: wp('5%')}}>
+                    <Text style={styles.textTotal}>Total</Text>
+                    <Text style={styles.textMoneyTotal}>R$ 10</Text>
+                </View>
+                <View style={styles.middleInferiorDivisor}/>
+                <View style={{justifyContent: 'center', marginLeft: wp('-10%')}}>
+                    <TouchableOpacity style={styles.confirmBackground}>
+                        <Text style={styles.confirmText}>Marcar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     </View>
     );
@@ -65,10 +165,10 @@ const styles = StyleSheet.create(
         fontSize: hp('3%'),
         textAlign: 'center'
     },
-    backgroundImage:{
+    backgroundArenaImage:{
         backgroundColor: '#378C4C',
-        width: '30%',
-        height: '15%',
+        width: wp('30%'),
+        height: hp('15%'),
         alignSelf: 'center',
         borderRadius: hp('6%')
     },
@@ -79,9 +179,68 @@ const styles = StyleSheet.create(
     },
     textArenaProps: {
         color: 'white',
-        fontSize: hp('2%')
-    }
+        fontSize: hp('2.8%'),
+        marginHorizontal: wp('2.5%'),
+    },
+    imageArenaProps: {
+        height: '100%',
+        width: '10%',
+        resizeMode: 'contain'
+    },
+    allItems: {
+        justifyContent: 'space-between',
+        flex: 3
+    },    
+    middleItems: {
+        backgroundColor: 'white',
+        width: '100%',
+        height: hp('40%'),
+        flex: 1
+    },
+
+    inferiorItems: {
+        flexDirection: 'row',
+        flex: 1/4,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '5%',
+    },
+    textTotal: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: hp('3%')
+    }, 
+    textMoneyTotal: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: hp('4%')
+    },
+    middleInferiorDivisor: {
+        height: '100%',
+        width: '1%',
+        borderRadius: hp('100%'),
+        backgroundColor: 'white',
+    },
+    confirmBackground: {
+        backgroundColor: '#378C4C',
+        height: hp('5%'),
+        paddingHorizontal: '5%',
+        borderRadius: wp('100%'),
+        justifyContent: 'center',
+    },
+    confirmText: {
+        color: 'white',
+        fontSize: hp('3%'),
+        textAlign: 'center'
+    },
 
 
 }
 );
+LocaleConfig.locales['br'] = {
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan.','Fev.','Mar.','Abr.','Mai.','Jun.','Jul.','Ago.','Set.','Out.','Nov.','Dec.'],
+    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+    dayNamesShort: ['Dom.','Seg.','Ter.','Qua.','Qui.','Sex.','Sáb.']
+  };
+  LocaleConfig.defaultLocale = 'br';
